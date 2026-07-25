f9a52655088004fdae8e1ddbf70789f782afb99b
asshahriar <asshahriark@gmail.com>
2026-07-26 02:47:25 +0600

    create account api done

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,1 +1,15 @@
-import accountModel from "../models/account.model";
+import accountModel from "../models/account.model.js";
+
+async function createAccountController(req, res) {
+	const user = req.user;
+
+	const account = await accountModel.create({
+		user: user._id,
+	});
+
+	res.status(201).json({
+		account,
+	});
+}
+
+export default { createAccountController };
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import authMiddleware from '../middleware/auth.middleware.js';
+import accountController from '../controllers/account.controller.js';
 
 const router = express.Router()
+
+/**
+ * - POST /api/accounts/
+ * - Create a new account
+ * - Protected Route
+ */
+
+router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
+
 
 
 export default router;
